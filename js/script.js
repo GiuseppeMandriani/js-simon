b890ba6a64a38317ded51231f763a8056f79a68d
GiuseppeMandriani <giuseppe.mandriani@gmail.com>
2021-04-12 14:06:24 +0200

    start

---
+++ js/script.js
@@ -0,0 +1,26 @@
+console.log('JS Ok');
+console.log('JQ', $);
+
+$(document).ready(function(){
+
+
+
+
+
+
+
+
+
+
+
+    // End Doc Ready
+})
+
+
+
+
+/**************************
+ * 
+ * UTILITIES / FUNCTIONS
+ * 
+ **************************/
